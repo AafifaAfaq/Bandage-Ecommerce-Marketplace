17cc998a8581c7f5418b38cec2b19855f0545de7
Aafifa Afaq <ak456fl789@gmail.com>
2024-12-20 14:31:08 +0500

    Add responsiveness

--- src/app/components/ProductMain.tsx
+++ src/app/components/ProductMain.tsx
@@ -9,44 +9,44 @@ import p5 from "@/app/assets/product-05.jpg";
 const ProductMain = () => {
   return (
     <section className="w-full px-4 py-8">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {/* Large Image on the Left */}
-        <div className="md:col-span-1">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 md:grid-cols-3">
+        {/* Large Image */}
+        <div className="sm:col-span-1">
           <Image
             src={p1}
             alt="Large Product"
-            className="w-full h-[1000px] rounded-md object-cover"
+            className="w-full h-[500px] sm:h-[400px] md:h-[600px] rounded-md object-cover"
           />
         </div>
 
-        {/* Smaller Images on the Right */}
-        <div className="grid grid-cols-2 gap-4 md:col-span-2">
+        {/* Smaller Images */}
+        <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 sm:gap-2 md:col-span-2">
           <div>
             <Image
               src={p2}
               alt="Product 2"
-              className="w-full h-[500px] rounded-md object-cover"
+              className="w-full h-[200px] sm:h-[300px] rounded-md object-cover"
             />
           </div>
           <div>
             <Image
               src={p3}
               alt="Product 3"
-              className="w-full h-auto rounded-md object-cover"
+              className="w-full h-[200px] sm:h-[300px] rounded-md object-cover"
             />
           </div>
           <div>
             <Image
               src={p4}
               alt="Product 4"
-              className="w-full h-auto rounded-md object-cover"
+              className="w-full h-[200px] sm:h-[300px] rounded-md object-cover"
             />
           </div>
           <div>
             <Image
               src={p5}
               alt="Product 5"
-              className="w-full h-auto rounded-md object-cover"
+              className="w-full h-[200px] sm:h-[300px] rounded-md object-cover"
             />
           </div>
         </div>
