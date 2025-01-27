381e5b2f7c0013490df5f5ee2673b31e51c58472
Aafifa Afaq <ak456fl789@gmail.com>
2025-01-28 00:07:56 +0500

    Added middleware for protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,15 +3,15 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isProtectedRoute = createRouteMatcher(['/checkout(.*)', '/login(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect()
+  if (isProtectedRoute(req)) {
+    // Protect routes that need authentication
+    await auth.protect()
+  }
 })
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    '/(api|trpc)(.*)', '/studio(.*)','/studio/:path*'   // Always run for API routes
   ],
 }
-
